681de54e922e5f101901a0b7f08ac5c8b8267703
meiuqer <denberghvanmartijn@gmail.com>
2016-06-03 16:57:29 +0200

    Update players.php

--- data/players.php
+++ data/players.php
@@ -3,9 +3,10 @@
 $players = [
     "FRA-ROM" => [
         "frederik" => "2-0",
+        "eddy wally" => "543-3,45"
     ],
     "ALB-SWI" => [
-        "frederik" => "1-1",
+        "frederik" => "1-1"
     ],
     "ROM-SWI" => [
         "frederik" => "1-2"
