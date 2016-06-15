dcf0c0e136ee98f4698d2eef5907774136a76a8a
Miguel Stevens <miguel@clouddesign.be>
2016-06-15 20:09:48 +0200

    Adding upcoming and previous match to the top

--- src/Collections/MatchCollection.php
+++ src/Collections/MatchCollection.php
@@ -10,26 +10,19 @@ class MatchCollection extends Collection {
     public function byClosest()
     {
         $items = $this->items;
-        foreach($items as $match) {
-            $intervals[] = (new \DateTime())->getTimestamp() - $match->getDate()->getTimestamp();
-        }
+        $previous = null;
 
-        $return = [];
-        foreach($intervals as $key => $interval) {
-            if($key != 0) {
-                if(($interval < $intervals[$key-1]) && ($interval > 0)) {
-                    $return[0] = $interval;
-                    $return[1] = $intervals[$key+1];
-                }
-                $return[] = $interval;
+        foreach($items as $code => $match) {
+            $timeago = (new \DateTime())->getTimestamp() - $match->getDate()->getTimestamp();
+            if($timeago < 0) {
+                moveUpInArray($items, makeMatchCode($previous));
+                moveUpInArray($items, makeMatchCode($match));
+                break;
             }
+            $previous = $match;
         }
 
-        // Get the closest number to zero but not below it
-        var_dump($intervals);
-        die();
-        asort($interval);
-        return key($interval);
-
+        return $items;
     }
+
 }
